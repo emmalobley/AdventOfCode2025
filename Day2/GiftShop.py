89fbc5481a82afc7079eabe1e69e9a0050833a1c
Emma <emmalobley3000@hotmail.co.uk>
2026-09-08 17:03:24 +0100

    Working part 1 of day 2

--- Day2/GiftShop.py
+++ Day2/GiftShop.py
@@ -1,12 +1,38 @@
-def get_ids(file_path):
+def get_id_ranges(file_path):
     # converts txt file to list or rotation instructions
     try:
-        stopword = open(file_path,"r")
-        lines = stopword.read().split('-')
+        file = open(file_path,"r")
+        id_ranges = file.read().split(',')
 
     except Exception as e:
         print(e)
 
-    return lines
+    return id_ranges
 
-print(get_ids("testinput.txt"))
+def get_all_even_digit_ids(id_ranges):
+    even_digit_ids = []
+    for r in id_ranges:
+        start, end = r.split('-')
+        if len(start)%2 == 0 or len(end)%2 == 0:
+            # one or both start/end ids in range is divisible by 2
+            new_ids = list(range(int(start),int(end)+1))
+            for id in new_ids:
+                if len(str(id))%2 == 0:
+                    even_digit_ids.append(str(id))
+
+    # print(even_digit_ids)
+    return even_digit_ids
+
+
+def get_sum_fake_ids(even_ids):
+    sum = 0
+    for s in even_ids:
+        first_half  = s[:len(s)//2]
+        second_half = s[len(s)//2:]
+        if first_half == second_half:
+            sum = sum + int(s)
+    return sum
+
+
+id_ranges = get_id_ranges("input.txt")
+print(get_sum_fake_ids(get_all_even_digit_ids(id_ranges)))
