7c75c9ed3d1cdd805ad6dcf4a0384ade0187e491
Emma <emmalobley3000@hotmail.co.uk>
2026-09-08 14:17:21 +0100

    attempt at part 2

--- Day1/SecretEntrance.py
+++ Day1/SecretEntrance.py
@@ -16,42 +16,46 @@ def add_orientation(x):
         return "-" + x[1:]
     return x[1:]
 
-def wrap_position(previous_position, x):
-    print(x)
+def wrap_position(previous_position, new_position):
+    # print(previous_position, new_position)
     i = 0
-    while x < 0:
-        if previous_position != 0:
-            i = i + 1
-        x = x + 100
-    while x > 99:
-        if x != 100:
-            i = i + 1
-        x = x - 100
+    
+    while new_position < 0:
+        i = i + 1
+        new_position = new_position + 100
+    while new_position > 99:
+        i = i + 1
+        new_position = new_position - 100
 
-    print(x,i)
-    return x, i
+    if previous_position == 0 & i > 0:
+            i = i - 1
+    print("New position: " + str(new_position))
+    print("Passed zero: "+ str(i))
+    return new_position, i
 
 def count_zeros(filename, start_position):
     lines = txt_to_lst(filename)
     position = start_position
-    zero_count = 0
-    i_count = 0
+    land_zero_count = 0
+    pass_zero_count = 0
     for line in lines:
         new_move = int(add_orientation(line))
+        print("New move: " + str(new_move))
         position, i = wrap_position(position, position + new_move)
         if position == 0:
-            zero_count = zero_count + 1
-        i_count = i_count + i
+            land_zero_count = land_zero_count + 1
+        pass_zero_count = pass_zero_count + i
         # print(position)
-    return zero_count, i_count
+    return land_zero_count, pass_zero_count
 
 
 # print(txt_to_lst("testinput.txt"))
 
-x, i = count_zeros("input.txt", 50)
+x, i = count_zeros("testinput.txt", 50)
 print("Zeros landed on: " + str(x))
 print("Zeros passed: " + str(i))
 print(x+i)
 
 # 969 Zeros landed on
 # 5483 too low
+# 5876 not right
